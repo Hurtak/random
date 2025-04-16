d349da7e0ead7187105802f1761d1a94e3fdf5ac
Petr Hurtak <petr.hurtak@gmail.com>
2025-04-16 22:25:53 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./", // use relative paths because we are hosting on GitHub Pages which hosts project in a nested directory
   server: {
     port: 3000,
   },
